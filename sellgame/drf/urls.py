9681b9ae10afff808ec60bc219454c025ee03520
Lolip0ps <lolipops-1@mail.ru>
2023-08-24 15:51:47 +0300

    Добавлен джосер (base and token)

--- sellgame/drf/urls.py
+++ sellgame/drf/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('v1/drf-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('v1/news/<int:pk>/', NewsAPIUpdate.as_view()),
     path('v1/newsdelete/<int:pk>/', NewsAPIDestroy.as_view()),
-    # path('api/v1/auth/', include('djoser.urls')),  # new
-    # re_path(r'^auth/', include('djoser.urls.authtoken')),  # new
+    path('v1/auth/', include('djoser.urls')),  # new
+    re_path(r'^auth/', include('djoser.urls.authtoken')),  # new
 ]
--- sellgame/sellgame/settings.py
+++ sellgame/sellgame/settings.py
@@ -55,6 +55,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'drf',
     'news',
     'catalog',
@@ -165,5 +167,11 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
 }
